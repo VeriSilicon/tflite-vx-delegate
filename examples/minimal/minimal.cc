be6fec0ef97d03b6adaee574e5f02714917adbe8
Zongwu Yang <zongwu.yang@verisilicon.com>
2021-10-14 11:00:11 +0800

    Refine minimal example
    Signed-off-by: Zongwu Yang <zongwu.yang@verisilicon.com>

--- examples/minimal/minimal.cc
+++ examples/minimal/minimal.cc
@@ -51,30 +51,16 @@ std::vector<T> read_data(const char * filename, size_t required)
     // Stop eating new lines in binary mode!!!
     file.unsetf(std::ios::skipws);
 
-    // get its size:
-    std::streampos fileSize;
-
-    file.seekg(0, std::ios::end);
-    fileSize = file.tellg();
-    file.seekg(0, std::ios::beg);
-
-    if(fileSize != required) {
-      std::cout << "Fatal: input size not matched" << std::endl;
-      return std::vector<T>();
-    }
-
     // reserve capacity
     std::vector<uint8_t> vec;
-    vec.reserve(fileSize);
+    vec.reserve(required);
 
     // read the data:
-    vec.insert(vec.begin(),
-               std::istream_iterator<uint8_t>(file),
-               std::istream_iterator<uint8_t>());
+    file.read(reinterpret_cast<char *>(vec.data()), required);
 
     std::vector<T> ret;
-    ret.reserve(fileSize/sizeof(T));
-    memcpy(ret.data(), vec.data(), fileSize);
+    ret.reserve(required/sizeof(T));
+    memcpy(ret.data(), vec.data(), required);
 
     return ret;
 }
@@ -85,7 +71,7 @@ float cosine(const std::vector<T>& lhs, const std::vector<T>& rhs) {
     float lhs_m = 0.0f;
 
     for(auto iter = lhs.begin(); iter != lhs.end(); ++iter) {
-      lhs_m += std::pow(*iter, 2);
+      lhs_m += *iter * (*iter);
     }
     lhs_m = std::sqrt(lhs_m);
 
@@ -118,6 +104,21 @@ void setupInput(int argc, char* argv[], const std::unique_ptr<tflite::Interprete
 
     std::cout << "Setup intput[" << std::string(interpreter->GetInputName(input_idx)) << "]" << std::endl;
     const char* input_data =  use_random_input ? "/dev/urandom" : argv[i];
+
+    if (!use_random_input) {
+      // get its size:
+      std::ifstream file(input_data, std::ios::binary);
+      std::streampos fileSize;
+
+      file.seekg(0, std::ios::end);
+      fileSize = file.tellg();
+      file.seekg(0, std::ios::beg);
+
+      if (fileSize != in_tensor->bytes) {
+        std::cout << "Fatal: input size not matched" << std::endl;
+        assert(false);
+      }
+    }
 
     switch (in_tensor->type) {
       case kTfLiteFloat32:
@@ -157,11 +158,6 @@ int main(int argc, char* argv[]) {
       tflite::FlatBufferModel::BuildFromFile(filename);
   TFLITE_MINIMAL_CHECK(model != nullptr);
 
-  // Prepare external delegate
-  // TfLiteExternalDelegateOptions options = {
-  //   .lib_path = argv[1], .count = 1, .keys = nullptr, .values = nullptr, .insert = nullptr
-  // };
-
   auto ext_delegate_option = TfLiteExternalDelegateOptionsDefault(argv[1]);
   auto ext_delegate_ptr = TfLiteExternalDelegateCreate(&ext_delegate_option);
 
@@ -171,33 +167,39 @@ int main(int argc, char* argv[]) {
   // tasks so that the Interpreter can read the provided model.
   tflite::ops::builtin::BuiltinOpResolver resolver;
   tflite::InterpreterBuilder builder(*model, resolver);
-  std::unique_ptr<tflite::Interpreter> interpreter;
-  std::unique_ptr<tflite::Interpreter> interpreter_cp;
-  builder(&interpreter);
-  builder(&interpreter_cp);
-  TFLITE_MINIMAL_CHECK(interpreter != nullptr);
-  interpreter->ModifyGraphWithDelegate(ext_delegate_ptr);
-  interpreter_cp->ModifyGraphWithDelegate(ext_delegate_ptr);
+  std::unique_ptr<tflite::Interpreter> npu_interpreter;
+  builder(&npu_interpreter);
+  TFLITE_MINIMAL_CHECK(npu_interpreter != nullptr);
+  npu_interpreter->ModifyGraphWithDelegate(ext_delegate_ptr);
 
   // Allocate tensor buffers.
-  TFLITE_MINIMAL_CHECK(interpreter->AllocateTensors() == kTfLiteOk);
-  TFLITE_MINIMAL_CHECK(interpreter_cp->AllocateTensors() == kTfLiteOk);
+  TFLITE_MINIMAL_CHECK(npu_interpreter->AllocateTensors() == kTfLiteOk);
   printf("=== Pre-invoke Interpreter State ===\n");
-  tflite::PrintInterpreterState(interpreter.get());
+  tflite::PrintInterpreterState(npu_interpreter.get());
 
   // Fill input buffers
   // TODO(user): Insert code to fill input tensors.
   // Note: The buffer of the input tensor with index `i` of type T can
   // be accessed with `T* input = interpreter->typed_input_tensor<T>(i);`
 
-  setupInput(argc, argv, interpreter);
-  setupInput(argc, argv, interpreter_cp);
+  setupInput(argc, argv, npu_interpreter);
 
   // Run inference
-  TFLITE_MINIMAL_CHECK(interpreter->Invoke() == kTfLiteOk);
-  TFLITE_MINIMAL_CHECK(interpreter_cp->Invoke() == kTfLiteOk);
+  TFLITE_MINIMAL_CHECK(npu_interpreter->Invoke() == kTfLiteOk);
+
+  // Get performance
+  {
+    const uint32_t loop_cout = 10;
+    auto start = std::chrono::high_resolution_clock::now();
+    for (uint32_t i = 0; i < loop_cout; i++) {
+      npu_interpreter->Invoke();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "[NPU Performance] Run " << loop_cout << " times, average time: " << (end - start).count() << " ms" << std::endl;
+  }
+
   printf("\n\n=== Post-invoke Interpreter State ===\n");
-  tflite::PrintInterpreterState(interpreter.get());
+  tflite::PrintInterpreterState(npu_interpreter.get());
 
   // CPU
   tflite::ops::builtin::BuiltinOpResolver cpu_resolver;
@@ -219,85 +221,35 @@ int main(int argc, char* argv[]) {
 
   // Run inference
   TFLITE_MINIMAL_CHECK(cpu_interpreter->Invoke() == kTfLiteOk);
+
+  // Get performance
+  {
+    const uint32_t loop_cout = 10;
+    auto start = std::chrono::high_resolution_clock::now();
+    for (uint32_t i = 0; i < loop_cout; i++) {
+      cpu_interpreter->Invoke();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "[CPU Performance] Run " << loop_cout << " times, average time: " << (end - start).count() << " ms" << std::endl;
+  }
+
   printf("\n\n=== Post-invoke CPU Interpreter State ===\n");
   tflite::PrintInterpreterState(cpu_interpreter.get());
 
-  if(0){// do run-sleep-run loop
-    const uint32_t loop_count = 10;
-    auto start = std::chrono::high_resolution_clock::now();
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::milliseconds sleep_{1000};
-
-    for(uint32_t i = 0; i < loop_count; ++i) {
-    auto start_0 = std::chrono::high_resolution_clock::now();
-      interpreter->Invoke();
-    auto end_0 = std::chrono::high_resolution_clock::now();
-      std::chrono::duration<double, std::milli> cost_0, cost_1;
-      cost_0 = end_0 - start_0;
-      std::cout << "[No switch]" << i << "  :cost = " << cost_0.count() << "ms" << std::endl;
-    }
-  }
-  if(0){// do run-sleep-run loop
-    const uint32_t loop_count = 10;
-    auto start = std::chrono::high_resolution_clock::now();
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::milliseconds sleep_{1000};
-
-    for(uint32_t i = 0; i < loop_count; ++i) {
-    auto start_0 = std::chrono::high_resolution_clock::now();
-      interpreter->Invoke();
-    auto end_0 = std::chrono::high_resolution_clock::now();
-    auto start_1 = std::chrono::high_resolution_clock::now();
-      interpreter_cp->Invoke();
-    auto end_1 = std::chrono::high_resolution_clock::now();
-
-      std::chrono::duration<double, std::milli> cost_0, cost_1;
-      cost_0 = end_0 - start_0;
-      cost_1 = end_1 - start_1;
-      std::cout << "[No Sleep]" << i << "  :cost 0 = " << cost_0.count() << "ms\t, cost 1 = " << cost_1.count() << "ms" << std::endl;
-    }
-  }
-  if(0){// do run-sleep-run loop
-    const uint32_t loop_count = 10;
-    auto start = std::chrono::high_resolution_clock::now();
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::milliseconds sleep_{1000};
-
-    for(uint32_t i = 0; i < loop_count; ++i) {
-    auto start_0 = std::chrono::high_resolution_clock::now();
-      interpreter->Invoke();
-    auto end_0 = std::chrono::high_resolution_clock::now();
-      std::this_thread::sleep_for(sleep_);
-    auto start_1 = std::chrono::high_resolution_clock::now();
-      interpreter_cp->Invoke();
-    auto end_1 = std::chrono::high_resolution_clock::now();
-
-      std::chrono::duration<double, std::milli> cost_0, cost_1;
-      cost_0 = end_0 - start_0;
-      cost_1 = end_1 - start_1;
-      std::cout << "[UseSleep] " << i << "  :cost 0 = " << cost_0.count() << "ms\t, cost 1 = " << cost_1.count() << "ms" << std::endl;
-    }
-  }
-
-  // Read output buffers
-  // TODO(user): Insert getting data out code.
-  // Note: The buffer of the output tensor with index `i` of type T can
-  // be accessed with `T* output = interpreter->typed_output_tensor<T>(i);`
-
-  auto output_idx_list = interpreter->outputs();
-  TFLITE_MINIMAL_CHECK(interpreter->outputs().size() == cpu_interpreter->outputs().size());
+  auto output_idx_list = npu_interpreter->outputs();
+  TFLITE_MINIMAL_CHECK(npu_interpreter->outputs().size() == cpu_interpreter->outputs().size());
   {  // compare result cosine similarity
     for (auto idx = 0; idx < output_idx_list.size(); ++idx) {
       // std::vector<float> result;
-      switch (interpreter->output_tensor(idx)->type) {
+      switch (npu_interpreter->output_tensor(idx)->type) {
         // case kTfLiteInt8:
         case kTfLiteUInt8: {
-          auto npu_out_buf = interpreter->typed_output_tensor<uint8_t>(idx);
+          auto npu_out_buf = npu_interpreter->typed_output_tensor<uint8_t>(idx);
           auto cpu_out_buf = cpu_interpreter->typed_output_tensor<uint8_t>(idx);
-          TFLITE_MINIMAL_CHECK(interpreter->output_tensor(idx)->bytes ==
+          TFLITE_MINIMAL_CHECK(npu_interpreter->output_tensor(idx)->bytes ==
                                cpu_interpreter->output_tensor(idx)->bytes);
 
-          auto bytes = interpreter->output_tensor(idx)->bytes;
+          auto bytes = npu_interpreter->output_tensor(idx)->bytes;
           for (auto j = 0; j < bytes; ++j) {
             if (std::abs(npu_out_buf[j] - cpu_out_buf[j]) > 2) {
               std::cout << "[Result mismatch]: Output[" << idx
@@ -308,56 +260,26 @@ int main(int argc, char* argv[]) {
             }
           }
 
-          {
-            std::vector<uint8_t> lhs(bytes);
-            auto lquant =
-                interpreter->tensor(output_idx_list[idx])->quantization;
-            std::vector<uint8_t> rhs(bytes);
-            // auto rquant =
-            //     interpreter->tensor(output_idx_list[idx])->quantization;
-            // auto dequantize = [](const std::vector<uint8_t>& quant_data,
-            //                      const TfLiteQuantization& quant_param)
-            //     -> std::vector<float> {
-            //   std::vector<float> ret;
+          std::vector<uint8_t> lhs(bytes);
+          auto lquant =
+              npu_interpreter->tensor(output_idx_list[idx])->quantization;
+          std::vector<uint8_t> rhs(bytes);
 
-            //   std::transform(
-            //       quant_data.cbegin(),
-            //       quant_data.cend(),
-            //       std::back_inserter(ret),
-            //       [&quant_param](const uint8_t& d) {
-            //         return ((TfLiteAffineQuantization*)quant_param.params)
-            //                    ->scale->data[0] *
-            //                (d - ((TfLiteAffineQuantization*)quant_param.params)
-            //                         ->zero_point->data[0]);
-            //       });
+          memcpy(lhs.data(), cpu_out_buf, bytes);
+          memcpy(rhs.data(), npu_out_buf, bytes);
 
-            //   return ret;
-            // };
-
-            memcpy(lhs.data(), cpu_out_buf, bytes);
-            memcpy(rhs.data(), npu_out_buf, bytes);
-
-            // std::vector<float> lhs_f;
-            // std::vector<float> rhs_f;
-
-            // lhs_f = dequantize(lhs, lquant);
-            // rhs_f = dequantize(rhs, rquant);
-
-            std::cout << "CosineCosineSimilarity = " << cosine(lhs, rhs)
-                      << std::endl;
-          //   std::cout << "CosineCosineSimilarity = " << cosine(lhs_f, rhs_f)
-          //             << std::endl;
-          }
+          std::cout << "CosineCosineSimilarity = " << cosine(lhs, rhs)
+                    << std::endl;
 
           break;
         }
         case kTfLiteFloat32: {
-          auto npu_out_buf = interpreter->typed_output_tensor<float>(idx);
+          auto npu_out_buf = npu_interpreter->typed_output_tensor<float>(idx);
           auto cpu_out_buf = cpu_interpreter->typed_output_tensor<float>(idx);
-          TFLITE_MINIMAL_CHECK(interpreter->output_tensor(idx)->bytes ==
+          TFLITE_MINIMAL_CHECK(npu_interpreter->output_tensor(idx)->bytes ==
                                cpu_interpreter->output_tensor(idx)->bytes);
 
-          auto bytes = interpreter->output_tensor(idx)->bytes;
+          auto bytes = npu_interpreter->output_tensor(idx)->bytes;
           for (auto j = 0; j < bytes / sizeof(float); ++j) {
             if (std::abs(npu_out_buf[j] - cpu_out_buf[j]) >
                 0.001f) {  // TODO{sven}: not accurate
@@ -367,16 +289,15 @@ int main(int argc, char* argv[]) {
             }
           }
 
-          if(0){
-            std::vector<float> lhs(bytes / sizeof(float));
-            std::vector<float> rhs(bytes / sizeof(float));
+          std::vector<float> lhs(bytes / sizeof(float));
+          std::vector<float> rhs(bytes / sizeof(float));
 
-            memcpy(lhs.data(), cpu_out_buf, bytes / sizeof(float));
-            memcpy(rhs.data(), npu_out_buf, bytes / sizeof(float));
+          memcpy(lhs.data(), cpu_out_buf, bytes / sizeof(float));
+          memcpy(rhs.data(), npu_out_buf, bytes / sizeof(float));
 
-            std::cout << "CosineCosineSimilarity = " << cosine(lhs, rhs)
-                      << std::endl;
-          }
+          std::cout << "CosineCosineSimilarity = " << cosine(lhs, rhs)
+                    << std::endl;
+
           break;
         }
         default: {
@@ -389,4 +310,3 @@ int main(int argc, char* argv[]) {
   TfLiteExternalDelegateDelete(ext_delegate_ptr);
   return 0;
 }
-
