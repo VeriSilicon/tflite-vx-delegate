7d7953f7ceba6b7f82e41c2e82f471b2d1a1300e
yuenan.li <yuenan.li@verisilicon.com>
2021-11-23 15:17:25 +0800

    build tim-vx and tflite as module in cmake
    Signed-off-by: yuenan.li <yuenan.li@verisilicon.com>

---
+++ cmake/modules/Findtim-vx.cmake
@@ -0,0 +1,43 @@
+#    Copyright (c) 2021 Vivante Corporation
+#
+#    Permission is hereby granted, free of charge, to any person obtaining a
+#    copy of this software and associated documentation files (the "Software"),
+#    to deal in the Software without restriction, including without limitation
+#    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#    and/or sell copies of the Software, and to permit persons to whom the
+#    Software is furnished to do so, subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be included in
+#    all copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#    DEALINGS IN THE SOFTWARE
+#
+if((NOT DEFINED TIM_VX_INSTALL))
+  include(FetchContent)
+  FetchContent_Declare(
+    tim-vx
+    GIT_REPOSITORY https://github.com/VeriSilicon/TIM-VX.git
+    GIT_TAG main
+  )
+  FetchContent_GetProperties(tim-vx)
+  if(NOT tim-vx_POPULATED)
+    FetchContent_Populate(tim-vx)
+  endif()
+  include_directories(${tim-vx_SOURCE_DIR}/include)
+  add_subdirectory("${tim-vx_SOURCE_DIR}" "${tim-vx_BINARY_DIR}" EXCLUDE_FROM_ALL)
+  target_link_libraries(vx_delegate tim-vx)
+else()
+  message("=== Building with TIM_VX_LIBRIRIES from ${TIM_VX_INSTALL} ===")
+  include_directories(${TIM_VX_INSTALL}/include)
+  set(LIBDIR lib)
+  if(CMAKE_SYSTEM_PROCESSOR STREQUAL aarch64)
+    set(LIBDIR lib64)
+  endif()
+  list(APPEND VX_DELEGATE_DEPENDENCIES ${TIM_VX_INSTALL}/${LIBDIR}/libtim-vx.so)
+endif()
