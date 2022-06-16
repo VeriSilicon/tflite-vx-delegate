1e035ce146191f097d9613d89e379255ed454f77
Chen Xin <jack.chen@verisilicon.com>
2022-06-16 15:04:52 +0800

    Fixed argmin kernel test
    Signed-off-by: Chen Xin <jack.chen@verisilicon.com>

--- op_map.cc
+++ op_map.cc
@@ -2098,6 +2098,9 @@ struct ArgOpMapper : public OpMapperBase<EmptyStructPlaceholder> {
       TFLITE_LOG_PROD(TFLITE_LOG_ERROR, "I8 input/I32 output is not supported");
       return false;
     }
+    if (context->tensors[node->inputs->data[0]].type == kTfLiteBool)  {
+        TFLITE_LOG_PROD(TFLITE_LOG_WARNING, "Bool input is converted to I8");
+    }
     if (0 == context->tensors[node->inputs->data[0]].dims->size ||
         0 == context->tensors[node->outputs->data[0]].dims->size) {
       TFLITE_LOG_PROD(TFLITE_LOG_ERROR, "Arg cannot support dynamic shape");
